3e6e6a3167931a9a896bc5cc61ea0924cc349b6b
Snega <snezhinka2809@mail.ru>
2025-04-10 12:10:23 +0300

    #6 Logout

--- navigation/AuthenticatedStackNavigator.tsx
+++ navigation/AuthenticatedStackNavigator.tsx
@@ -1,13 +1,18 @@
+import { useContext } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { AuthContext } from '../store/auth-context';
 import { Colors } from '../constants/styles';
 import { AuthenticatedStackParamList } from './types';
 
 import { WelcomeScreen } from '../screens/WelcomeScreen';
+import { IconButton } from '../components/ui/IconButton';
 
 const Stack = createNativeStackNavigator<AuthenticatedStackParamList>();
 
 export const AuthenticatedStackNavigator = () => {
+  const authCtx = useContext(AuthContext);
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -16,7 +21,20 @@ export const AuthenticatedStackNavigator = () => {
         contentStyle: { backgroundColor: Colors.primary100 },
       }}
     >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{
+          headerRight: ({ tintColor }) => (
+            <IconButton
+              icon="exit"
+              color={tintColor || Colors.primary100}
+              size={24}
+              onPress={authCtx.logout}
+            />
+          ),
+        }}
+      />
     </Stack.Navigator>
   );
 };
